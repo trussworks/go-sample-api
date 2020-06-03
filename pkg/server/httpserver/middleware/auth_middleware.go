ae8c17896e849d9bc2320a426d95454a51e33df5
Mikena Wood <mikena@truss.works>
2020-06-03 16:32:10 -0700

    add fake authorization middleware to populate context

---
+++ pkg/server/httpserver/middleware/auth_middleware.go
@@ -0,0 +1,32 @@
+package httpserver
+
+import (
+	"net/http"
+
+	"bin/bork/pkg/apis/v1/http/handlers"
+	"bin/bork/pkg/appcontext"
+	"bin/bork/pkg/apperrors"
+	"bin/bork/pkg/models"
+)
+
+type FakeAuthorizeMiddlewareFactory struct {
+	base handlers.HandlerBase
+}
+
+func (m FakeAuthorizeMiddlewareFactory) authorizeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			m.base.WriteErrorResponse(r.Context(), w, &apperrors.UnauthorizedError{})
+		}
+		ctx := appcontext.WithUser(r.Context(), models.User{ID: authHeader})
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
+
+// NewFakeAuthorizeMiddleware does some fake authorization
+func NewFakeAuthorizeMiddleware(base handlers.HandlerBase) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return FakeAuthorizeMiddlewareFactory{base: base}.authorizeMiddleware(next)
+	}
+}
--- pkg/server/httpserver/routes.go
+++ pkg/server/httpserver/routes.go
@@ -5,15 +5,23 @@ import (
 
 	"bin/bork/pkg/apis/v1/http/handlers"
 	"bin/bork/pkg/models"
+	httpserver "bin/bork/pkg/server/httpserver/middleware"
 	"bin/bork/pkg/services"
 )
 
 func (s *Server) routes() {
 	api := s.router.PathPrefix("/api/v1").Subrouter()
 
+	// set up base handler
 	handlerBase := handlers.NewHandlerBase(s.logger)
 
+	// use authorization on API
+	api.Use(httpserver.NewFakeAuthorizeMiddleware(handlerBase))
+
+	// set up service factory
 	serviceFactory := services.NewServiceFactory(s.logger)
+
+	// endpoint for dog
 	fakeAuthorize := func(user models.User, dog *models.Dog) (bool, error) {
 		return true, nil
 	}
@@ -21,7 +29,6 @@ func (s *Server) routes() {
 		return &models.Dog{}, nil
 	}
 
-	// endpoint for dog
 	dogHandler := handlers.NewDogHandler(handlerBase, serviceFactory.NewFetchDog(fakeAuthorize, fakeFetchDog))
 	api.Handle("/dog/{dog_id}", dogHandler.Handle())
 }
